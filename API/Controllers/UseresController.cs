0ca9b82991fc85df0eaed8a6cbb97b90e29dde68
Mahmoud101saad <73835523+Mahmoud101saad@users.noreply.github.com>
2023-10-25 12:06:56 +0200

    End Of Section 2

---
+++ API/Controllers/UseresController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UseresController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UseresController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() { 
+        
+            return await _dataContext.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+    : base(options)
+        { }
+        public DbSet<AppUser>Users { get; set; }
+    }
+}
